26833440ab801d9353146c3f47bfad073a942576
acastror26 <aurivancastror@gmail.com>
2024-07-05 19:07:21 -0400

    Added schemas

---
+++ src/api/schemas/schemas.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: Optional[str] = None
+    country: Optional[str] = None
+    state: Optional[str] = None
+    city: Optional[str] = None
+    is_active: Optional[bool] = True
+    is_m2m_account: Optional[bool] = False
+
+class UserCreate(UserBase):
+    password: str
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenRequest(BaseModel):
+    email: str
+    password: str
+
+class TokenData(BaseModel):
+    email: str
+    is_active: bool
